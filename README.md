0672db0f2c411a599e9b43ab08055efb11516f3a
LeonardParker5 <leonard.parker5@gmail.com>
2023-12-27 14:27:00 -0500

    Day01 solution

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # AdventofCode2017
-Advent of Code 2017 solutions
+
+Advent of Code 2017 solutions in Rust
+
+### Runtime
+
+| Day   | Part 1 | Part 2 |
+| ----- | ------ | ------ |
+| Day 1 | 0.20s  | 0.21s  |
---
+++ day01/src/main.rs
@@ -0,0 +1,36 @@
+// ADVENT OF CODE 2017 - DAY 01
+
+use std::fs;
+
+fn main() {
+    // PART 1
+    let input = fs::read_to_string("../Inputs/day01input.txt").expect("Error");
+    let list: Vec<u32> = input.chars().map(|c| c.to_digit(10).unwrap()).collect();
+
+    let mut sum: u32 = 0;
+    
+    for window in list.windows(2) {
+        if window[0] == window[1] { sum += window[0] }
+    }
+    if list[list.len() - 1] == list[0] { sum += list[0] }
+
+    println!("{}", sum);
+
+    //PART 2
+
+    sum = 0;
+    let half: u32 = (list.len() / 2).try_into().unwrap();
+    let length: u32 = (list.len()).try_into().unwrap();
+    
+    for i in 0..length {
+        let jump;
+        if i + (half) > (length - 1) {
+            jump = (i + (half)) - (length);
+        } else {
+            jump = i + (half)
+        }
+        if list[i as usize] == list[jump as usize] { sum += list[i as usize] }
+    }
+
+    println!("{}", sum);
+}
